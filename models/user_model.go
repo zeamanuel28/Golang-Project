652681a210c1b54145be4cf28d63101049832e70
zeamanuel28 <zeamanuelnegussie24@gmail.com>
2025-06-11 09:39:30 -0700

    autentication

--- models/user_model.go
+++ models/user_model.go
@@ -2,10 +2,17 @@ package models
 
 import "gorm.io/gorm"
 
+// Name struct holds first and last names
+type Name struct {
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+}
+
 // User represents the user model
 type User struct {
-	gorm.Model        // Provides ID, CreatedAt, UpdatedAt, DeletedAt
-	Username   string `gorm:"unique;not null" json:"username" binding:"required"`
-	Email      string `gorm:"unique;not null" json:"email" binding:"required,email"`
-	Password   string `json:"password" binding:"required,min=6"`
+	gorm.Model
+	Name     *Name  `gorm:"embedded;embeddedPrefix:name_" json:"name" binding:"required"`
+	Username string `gorm:"unique;not null" json:"username" binding:"required"`
+	Email    string `gorm:"unique;not null" json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
 }
